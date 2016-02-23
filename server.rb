7cb2eebf9967a66498c61747cfc9acf3ac5831df
Joshfindit <linkingparents@gmail.com>
2016-02-23 16:48:16 -0400

    First code for making and receiving calls through Slack.

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ require 'addressable/uri'
 require 'twilio-ruby'
 require 'yaml'
 
-configFile = YAML::load_file(File.join(__dir__, 'config.yml'))
+configFile = YAML::load_file(File.join(__dir__, '_config.yml'))
 
 configure do
   set :serverBaseURL, configFile['serverBaseURL']
@@ -18,16 +18,16 @@ configure do
   set :slackSlashCommandTokenSMS, configFile['slackSlashCommandTokenSMS']
   set :slackSlashCommandTokenTXT, configFile['slackSlashCommandTokenTXT']
   set :slackSlashCommandTokenS, configFile['slackSlashCommandTokenS']
-  # set :slackSlashCommandTokenCall, configFile['slackSlashCommandTokenCall']
-  # set :slackSlashCommandTokenCallFrom, configFile['slackSlashCommandTokenCallFrom']
+  set :slackSlashCommandTokenCall, configFile['slackSlashCommandTokenCall']
+  set :slackSlashCommandTokenCallFrom, configFile['slackSlashCommandTokenCallFrom']
   set :twilioAccountSID, configFile['twilioAccountSID']
   set :twilioAuthToken, configFile['twilioAuthToken']
   set :twilioOutsideNumber, configFile['twilioOutsideNumber']
-  # set :twilioSecondOutsideNumber, configFile['twilioSecondOutsideNumber']
+  set :twilioSecondOutsideNumber, configFile['twilioSecondOutsideNumber']
   set :myCurrentNumber, configFile['myCurrentNumber']
 end
 
-use Rack::TwilioWebhookAuthentication, settings.twilioAuthToken, '/Twilio_Slack/incomingsms' #verifies that only Twilio can post to the 'incomingsms' url. As per http://thinkvoice.net/twilio-sms-forwarding/
+use Rack::TwilioWebhookAuthentication, settings.twilioAuthToken, '/Twilio_Slack/incoming' #verifies that only Twilio can post to the 'incoming' url. As per http://thinkvoice.net/twilio-sms-forwarding/
 
 set :port, 8080
 set :environment, :production
@@ -37,7 +37,7 @@ error Sinatra::NotFound do
   [404, 'Not Found']
 end
 
-def sanitize_number (string) # Returns only digits
+def sanitize_number (string)
    if sanitized = string.gsub(/[^\d]/, '') #watch out: if no gsub match, ruby freaks out
      return sanitized
    else
@@ -84,6 +84,71 @@ def setChannelPurpose(channelID, purpose) #Currently does not work. Vestigial an
 end
 
 
+## Needs testing
+def startConference(to, from, myNumber) #"to" is the recipient. myNumber is the caller #Needs testing
+  begin
+    @client = Twilio::REST::Client.new settings.twilioAccountSID, settings.twilioAuthToken
+    @call = @client.account.calls.create(
+      :from => "+#{from}",   # From your Twilio number
+      #:to => '+#{myCurrentPhoneNumber}',     # To any number
+      :to => "+#{myNumber}", #to me
+      :timeout => 7, #Only try me for 7 seconds
+      #:record => true, #creates a second recording? Yes.
+      # Fetch instructions from this URL when the call connects
+      :url => "#{settings.serverBaseURL}/Twilio_Slack/startconferenceroom?room=#{to}",
+#    :status_callback => "#{settings.serverBaseURL}/Twilio_Slack/callstatus?number=#{to}",
+#    :status_callback_method => "POST",
+#    :status_callback_event => ["initiated", "ringing", "answered", "completed"],
+    )
+    puts "Creating the conference by calling the first participant (#{myNumber}): #{@call.status}"
+  rescue Twilio::REST::RequestError => e
+    #If Twilio reports an error, rescue
+    puts "Twilio reports call failed: #{e.message}"
+    postMessage(params[:channel_name], "CALL command failed. Twilio reports:e #{e.message}")
+  else
+    #Send was successful (probably)
+    #postMessage(params[:channel_name], "#{params[:user_name]} sent SMS: #{params[:text]}")
+    puts "Called through Twilio successfully (to me)"
+  end
+end
+## /Needs testing
+
+
+def joinConference(to, from) #Note: the from number has to be different than the one used for the started conference
+  begin
+    @client = Twilio::REST::Client.new settings.twilioAccountSID, settings.twilioAuthToken
+    @call = @client.account.calls.create(
+      :from => "+#{from}",   # From your Twilio number
+      :to => "+#{to}", #the recipient
+      :record => true, #creates a second recording?
+      # Fetch instructions from this URL when the call connects
+      :url => "#{settings.serverBaseURL}/Twilio_Slack/outgoingconferenceroom?room=#{to}",
+      :status_callback => "#{settings.serverBaseURL}/Twilio_Slack/callstatus?number=#{to}",
+      :status_callback_method => "POST",
+      :status_callback_event => ["completed"]
+    )
+    puts "Joining #{to} to the conference: #{@call.status}"
+  rescue Twilio::REST::RequestError => e
+    #If Twilio reports an error, rescue
+    puts "Twilio reports call failed: #{e.message}"
+    postMessage(params[:channel_name], "CALL command failed. Twilio reports:e #{e.message}")
+  else
+    #Send was successful (probably)
+    puts "Called through Twilio successfully (to #{to})"
+  end
+end
+
+
+def conferenceInfo() #unfinished.
+  #Finding information about conference rooms
+  @client = Twilio::REST::Client.new settings.twilioAccountSID, settings.twilioAuthToken
+  @client.account.conferences.list({ :friendly_name => probablePhoneNumber}).each do |conference|
+    puts "found conference before creation: #{conference.friendly_name}. #{conference.status}."
+    #p conference
+  end
+end
+
+
 post '/Twilio_Slack/forwardtocurrentnumber' do # No frills: Forwards the call to myCurrentNumber
   puts "received to /Twilio_Slack/forwardtocurrentnumber"
   p params
@@ -113,9 +178,9 @@ post '/Twilio_Slack/forwardtocurrentnumberwithtones' do # With frills: Forwards 
 end
 
 
-post '/Twilio_Slack/incomingsms' do #Received an SMS from Twilio; post to Slack
+post '/Twilio_Slack/incoming' do #Received an SMS from Twilio; post to Slack
 
-  puts "POST request received to /Twilio_Slack/incomingsms : an SMS was sent to the external Twilio number"
+  puts "POST request received to /Twilio_Slack/incoming : an SMS was sent to the external Twilio number"
   # Handy debug stuff: 
   # p params
   # puts "From: #{params[:From]}"
@@ -159,10 +224,10 @@ post '/Twilio_Slack/incomingsms' do #Received an SMS from Twilio; post to Slack
 end
 
 
-post '/Twilio_Slack/outgoingsms' do #Sending an SMS from Slack to Twilio
+post '/Twilio_Slack/outgoing' do #Sending an SMS from Slack to Twilio
 #  p params
 
-  puts "POST request recieved to /Twilio_Slack/outgoingsms . Time to send an SMS."
+  puts "POST request recieved to /Twilio_Slack/outgoing . Time to send an SMS."
 
   # Check if the API key matches one that's allowed
   if params[:token] == settings.slackSlashCommandTokenSMS || params[:token] == settings.slackSlashCommandTokenTXT || params[:token] == settings.slackSlashCommandTokenS
@@ -206,6 +271,141 @@ post '/Twilio_Slack/outgoingsms' do #Sending an SMS from Slack to Twilio
   end
 end
 
+
+post '/Twilio_Slack/outgoingphonecall' do
+#  p params
+
+  puts "POST request recieved to /Twilio_Slack/outgoingphonecall. Calling the forwarding number (settings.myCurrentNumber)"
+  if params[:token] == settings.slackSlashCommandTokenCall || params[:token] == settings.slackSlashCommandTokenCallFrom
+    puts "channel_id: #{params[:channel_name]}"
+    puts "manualNumber: #{params[:text]}"
+    #    puts sanitize_number(params[:channel_name])
+
+    # strip sms from params[:channel_name]
+    if potentialPhoneNumber = sanitize_number(params[:channel_name]) #Get the outgoing number
+      # Check if it's an 11 digit number
+      #      puts "potentialPhoneNumber: #{potentialPhoneNumber}"
+      #      puts "potentialPhoneNumber.length: #{potentialPhoneNumber.length}"
+      if potentialPhoneNumber.length == 11
+        probablePhoneNumber = potentialPhoneNumber
+        puts "Phone number (probably) detected: #{probablePhoneNumber}"
+      else
+        #Send a message to the channel stating that a phone number wasn't found
+        postMessage(params[:channel_name], "CALL command failed - the channel name does not contain an 11 digit number.")
+        halt
+      end
+    end
+
+    if params[:text] != '' #Get my number
+      myPotentialPhoneNumber = params[:text]
+    else
+      myPotentialPhoneNumber = settings.myCurrentNumber
+    end
+
+    if myPotentialPhoneNumber.length == 11
+      # Call from Twilio - my phone first
+      myCurrentPhoneNumber = myPotentialPhoneNumber
+      puts "Dialing #{myCurrentPhoneNumber}"
+      postMessage(params[:channel_name], "Calling you at #{myCurrentPhoneNumber}.")
+      startConference(probablePhoneNumber, settings.twilioSecondOutsideNumber, myCurrentPhoneNumber)
+
+      #Wait for the channel to be created. For now, we'll just sleep
+      sleep 5
+      i = 0
+      while i < 11 do #wait 11 seconds before trying not to do the second call. Note: Conference is marked as in-progress after 17 seconds?
+                      #Turns out the call went to voicemail, which was marked as in progress. This complicates things. Timeout
+        puts "Waiting. i = #{i}"
+        sleep 1
+        conferenceListStatus = @client.account.conferences.list({
+          :status => "in-progress",
+          :friendly_name => "#{probablePhoneNumber}"}).each do |conference|
+          # if conference.status == "in-progress"
+            puts "Found in-progress conference - calling the second number"
+            postMessage(params[:channel_name], "You answered (probably). Calling the recipient at #{probablePhoneNumber}.")
+            puts "Now doing joinConference(#{probablePhoneNumber}, #{settings.twilioOutsideNumber})"
+            joinConference(probablePhoneNumber, settings.twilioOutsideNumber)
+            postMessage("twilio-status", "Made a call between caller at #{myCurrentPhoneNumber} and recipient at #{probablePhoneNumber}.")
+            i = 20
+            break
+          # else
+            #puts conference.status
+          # end
+        end
+        i +=1
+      end
+      puts "conferenceListStatus: #{conferenceListStatus}"
+      # joinConference(probablePhoneNumber, settings.twilioOutsideNumber)
+
+    else
+      #Send a message to the channel stating that a phone number wasn't found
+      postMessage(params[:channel_name], "CALL command failed - '#{myPotentialPhoneNumber}' number is not 11 digits. Try /callfrom")
+      halt
+    end
+
+    #puts "Twiml = #{settings.serverBaseURL}/Twilio_Slack/outgoingconferenceroom?room=#{probablePhoneNumber}"
+  end
+  #@client = Twilio::REST::Client.new settings.twilioAccountSID, settings.twilioAuthToken
+end
+
+
+
+post '/Twilio_Slack/startconferenceroom' do
+  #p params
+  #room = params[:room]
+  
+  twiml = "<?xml version='1.0' encoding='UTF-8'?><Response><Say>Connecting</Say><Dial><Conference record='record-from-start' beep='true' startConferenceOnEnter='true' endConferenceOnExit='true'>#{params[:room]}</Conference></Dial></Response>"
+
+  content_type 'text/xml'
+
+  twiml
+end
+
+
+post '/Twilio_Slack/outgoingconferenceroom' do
+  #p params
+  #room = params[:room]
+
+  twiml = "<?xml version='1.0' encoding='UTF-8'?><Response><Say></Say><Dial><Conference record='record-from-start' beep='false' waitUrl='' startConferenceOnEnter='true' endConferenceOnExit='true'>#{params[:room]}</Conference></Dial></Response>"
+  # "<?xml version='1.0' encoding='UTF-8'?><Response><Say>Connecting</Say><Dial><Conference beep='true' endConferenceOnExit='false'>#{params[:room]}</Conference></Dial></Response>"
+
+  content_type 'text/xml'
+
+  twiml
+end
+
+
+post '/Twilio_Slack/callstatus' do
+  puts "Request received on /Twilio_Slack/callstatus"
+  p params
+
+#Example completed post:
+##{"Called"=>"+18885551212", "ToState"=>"", "CallerCountry"=>"US",
+#  "Direction"=>"inbound", "Timestamp"=>"Sun, 08 Nov 2015 18:49:31 +0000",
+#  "CallbackSource"=>"call-progress-events", "CallerState"=>"NC", "ToZip"=>"",
+#  "SequenceNumber"=>"0", "CallSid"=>"CA7b9df41d0dIOAd1f76fafebe844105ab4",
+#  "To"=>"+18885551212", "CallerZip"=>"28301", "CallerName"=>"FAYETTEVILL  NC",
+#  "ToCountry"=>"US", "ApiVersion"=>"2010-04-01", "CalledZip"=>"", "CalledCity"=>"",
+#  "CallStatus"=>"completed", "Duration"=>"1", "From"=>"+19105285448", "CallDuration"=>"17",
+#  "AccountSid"=>"AC7a7689e38d69622e332c0e65690c9b5e", "CalledCountry"=>"US",
+#  "CallerCity"=>"FAYETTEVILLE", "Caller"=>"+19105285448", "FromCountry"=>"US",
+#  "ToCity"=>"", "FromCity"=>"FAYETTEVILLE", "CalledState"=>"", "FromZip"=>"28301", "FromState"=>"NC"}
+
+  if params[:CallStatus] == "completed"
+    if params[:number] #if '?number' is specified (the recipient) post to the the correct channel
+      puts "sanitize_number(params[:number]) #{sanitize_number(params[:number])}. params[:number]: #{params[:number]}"
+      postMessage("sms#{sanitize_number(params[:number])}", "**New #{params[:Duration]}min recording** from #{params[:number]}.\nRecording link: #{params[:RecordingUrl]}.wav")
+    else #channel not specified. must be an incoming call. Post to Twilio-status
+      puts "?number not specified. must be an incoming call."
+      if params[:RecordingUrl]
+        postMessage("twilio-status", "**New #{params[:Duration]}min recording** from #{params[:number]} (#{params[:Caller]}, #{params[:CallerName]}).\nRecording link: #{params[:RecordingUrl]}.wav")
+      else
+        postMessage("twilio-status", "**incoming call at #{params[:Timestamp]} from #{params[:From]} (#{params[:CallerName]})")
+      end
+    end
+  else
+    puts "Called /callstatus with a non-'completed' status"
+  end
+end
 
 post '/Twilio_Slack/voicemail' do
   # 
